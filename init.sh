f8285258a7ed9507d83e23b317874b210306ec34
Christian Wimmer <wimmer@teamchris.info>
2024-10-18 23:06:52 +0200

    action

--- init.sh
+++ init.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 set -ex
 
+node -v
+
+rm package-lock.json
 npm install
+
 cd .ops
+
 cd hbscli
+rm package-lock.json
 npm install
+
 cd ../headliner
+rm package-lock.json
 npm install
+
 cd ../glossary
+rm package-lock.json
 npm install
+
 cd ../statistics
+rm package-lock.json
 npm install
