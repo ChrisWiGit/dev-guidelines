e7ba5d91d5f736eeb4c6ee31681a61d0b620b747
Christian Wimmer <none@example.com>
2025-01-18 11:01:38 +0100

    init Skript erweitert.

--- init.sh
+++ init.sh
@@ -3,29 +3,41 @@ set -ex
 
 node -v
 
-rm package-lock.json
+echo "Base"
+rm package-lock.json || true
 npm install
+npm audit fix --force
 
 cd .ops
 
+echo "HBS"
 cd hbscli
-rm package-lock.json
+rm package-lock.json || true
 npm install
+npm audit fix --force
 
+echo "headliner"
 cd ../headliner
-rm package-lock.json
+rm package-lock.json || true
 npm install
+npm audit fix --force
 
+echo "glossary"
 cd ../glossary
-rm package-lock.json
+rm package-lock.json || true
 npm install
+npm audit fix --force
 
+echo "statistics"
 cd ../statistics
-rm package-lock.json
+rm package-lock.json || true
 npm install
+npm audit fix --force
 
+echo "todos"
 cd ../todos
-rm package-lock.json
+rm package-lock.json || true
 npm install
+npm audit fix --force
 
 cd ../..
