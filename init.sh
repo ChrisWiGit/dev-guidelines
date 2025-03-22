4e341cc47b45a0d56698dcbe38cfb10f73a7c5ba
Christian Wimmer <12424397+ChrisWiGit@users.noreply.github.com>
2025-03-22 21:39:45 +0100

    Skript fails

--- init.sh
+++ init.sh
@@ -5,39 +5,45 @@ node -v
 
 echo "Base"
 rm package-lock.json || true
+rm -rf node_modules || true
 npm install
-npm audit fix --force
+npm audit fix || true
 
 cd .ops
 
 echo "HBS"
 cd hbscli
 rm package-lock.json || true
+rm -rf node_modules || true
 npm install
-npm audit fix --force
+npm audit fix || true
 
 echo "headliner"
 cd ../headliner
 rm package-lock.json || true
+rm -rf node_modules || true
 npm install
-npm audit fix --force
+npm audit fix || true
 
 echo "glossary"
 cd ../glossary
 rm package-lock.json || true
+rm -rf node_modules || true
 npm install
-npm audit fix --force
+npm audit fix || true
 
 echo "statistics"
 cd ../statistics
 rm package-lock.json || true
+rm -rf node_modules || true
 npm install
-npm audit fix --force
+npm audit fix || true
 
 echo "todos"
 cd ../todos
+rm -rf node_modules || true
 rm package-lock.json || true
 npm install
-npm audit fix --force
+npm audit fix || true
 
 cd ../..
